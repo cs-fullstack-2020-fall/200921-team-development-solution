a278e9f34c5c514239fca18051a7c676be335d0e
autumn-ragland <raglanda998@gmail.com>
2020-09-21 11:43:58 -0500

    add context to services and gen db

--- ProductCatolog/Startup.cs
+++ ProductCatolog/Startup.cs
@@ -5,9 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using ProductCatolog.DAO;
 
 namespace ProductCatolog
 {
@@ -23,7 +25,8 @@ namespace ProductCatolog
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+             services.AddDbContext<ProductDbContext>(options => options.UseSqlite(Configuration.GetConnectionString("DefaultDbConnection")));
+            services.AddControllersWithViews().AddRazorRuntimeCompilation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
