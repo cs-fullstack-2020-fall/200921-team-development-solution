ce750f953e25c0903b84b9bab7b6526df93d7647
autumn-ragland <raglanda998@gmail.com>
2020-09-21 12:09:10 -0500

    add comments and ref to db

--- ProductCatolog/Controllers/ProductController.cs
+++ ProductCatolog/Controllers/ProductController.cs
@@ -1,17 +1,27 @@
 using Microsoft.AspNetCore.Mvc;
+using ProductCatolog.DAO;
 
 namespace ProductCatolog.Controllers
 {
     public class Product : Controller
     {
+        // ref to db
+        private readonly ProductDbContext _context;
+        public Product(ProductDbContext context)
+        {
+            _context = context;
+        }
+        // list all
         public IActionResult Index()
         {
             return Content("List All");
         }
+        // display form to add
         public IActionResult CreateForm()
         {
             return Content("Create Product");
         }
+        // add to db
         public IActionResult Add()
         {
             return Content("Add Product to DB");
